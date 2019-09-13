e1c30268b3f002af87be91b7056214022ef0a1d4
Jorge Quiros <jorge.quiros@scm.ca>
2019-09-13 11:19:04 -0600

    adding

--- conf/conf.js
+++ conf/conf.js
@@ -1,4 +1,13 @@
 // An example configuration file.
+
+// This is for screenshot
+var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+
+var reporter = new HtmlScreenshotReporter({
+  dest: 'target/screenshots',
+  filename: 'my-report.html'
+});
+
 exports.config = {
 
   //to avoid starting the selenium server separately
@@ -19,5 +28,25 @@ exports.config = {
   // Options to be passed to Jasmine.
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
+  },
+
+  // Setup the report before any tests start
+  beforeLaunch: function() {
+    return new Promise(function(resolve){
+      reporter.beforeLaunch(resolve);
+    });
+  },
+
+  // Assign the test reporter to each running instance
+  onPrepare: function() {
+    jasmine.getEnv().addReporter(reporter);
+  },
+
+  // Close the report after all tests finish
+  afterLaunch: function(exitCode) {
+    return new Promise(function(resolve){
+      reporter.afterLaunch(resolve.bind(this, exitCode));
+    });
   }
+
 };
