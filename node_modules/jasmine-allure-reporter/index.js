60263849001a6f101645138a26b9c0bec26d3e61
Jorge Quiros <jorge.quiros@scm.ca>
2019-09-18 11:13:08 -0600

    removing not necessary folders

--- node_modules/jasmine-allure-reporter/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var Runtime = require('allure-js-commons/runtime');
-var Jasmine2AllureReporter = require('./src/Jasmine2AllureReporter.js');
-var allureReporter = Jasmine2AllureReporter.allureReporter;
-
-module.exports = Jasmine2AllureReporter.Jasmine2AllureReporter;
-global.allure = new Runtime(allureReporter);
